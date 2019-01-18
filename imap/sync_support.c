fb2f1936588a3cf69ceafcad55a68d8a545bb3a5
Ken Murchison <murch@fastmail.com>
2019-01-18 15:35:44 -0500

    sync_support.c: introduced dlist_getname_internal() which includes mboxname_from_standard() translation

--- imap/sync_support.c
+++ imap/sync_support.c
@@ -2348,6 +2348,17 @@ redo:
     mailbox_close(&mailbox);
 }
 
+static int dlist_getname_internal(struct dlist *parent, const char *name,
+                                  char **mboxnamep)
+{
+    const char *stdname;
+
+    if (!dlist_getatom(parent, name, &stdname)) return 0;
+
+    *mboxnamep = mboxname_from_standard(stdname);
+    return 1;
+}
+
 int sync_apply_reserve(struct dlist *kl,
                        struct sync_reserve_list *reserve_list,
                        struct sync_state *sstate)
@@ -3595,7 +3606,7 @@ int sync_get_sieve(struct dlist *kin, struct sync_state *sstate)
  * so just pick the file out from under the hood */
 int sync_get_message(struct dlist *kin, struct sync_state *sstate)
 {
-    const char *mboxname;
+    char *mboxname;
     const char *partition;
     const char *uniqueid;
     const char *guid;
@@ -3604,10 +3615,7 @@ int sync_get_message(struct dlist *kin, struct sync_state *sstate)
     struct dlist *kl;
     struct message_guid tmp_guid;
     struct stat sbuf;
-    char *intname;
 
-    if (!dlist_getatom(kin, "MBOXNAME", &mboxname))
-        return IMAP_PROTOCOL_BAD_PARAMETERS;
     if (!dlist_getatom(kin, "PARTITION", &partition))
         return IMAP_PROTOCOL_BAD_PARAMETERS;
     if (!dlist_getatom(kin, "UNIQUEID", &uniqueid))
@@ -3618,18 +3626,18 @@ int sync_get_message(struct dlist *kin, struct sync_state *sstate)
         return IMAP_PROTOCOL_BAD_PARAMETERS;
     if (!message_guid_decode(&tmp_guid, guid))
         return IMAP_PROTOCOL_BAD_PARAMETERS;
+    if (!dlist_getname_internal(kin, "MBOXNAME", &mboxname))
+        return IMAP_PROTOCOL_BAD_PARAMETERS;
 
-    intname = mboxname_from_standard(mboxname);
-
-    fname = mboxname_datapath(partition, intname, uniqueid, uid);
+    fname = mboxname_datapath(partition, mboxname, uniqueid, uid);
     if (stat(fname, &sbuf) == -1) {
-        fname = mboxname_archivepath(partition, intname, uniqueid, uid);
+        fname = mboxname_archivepath(partition, mboxname, uniqueid, uid);
         if (stat(fname, &sbuf) == -1) {
-            free(intname);
+            free(mboxname);
             return IMAP_MAILBOX_NONEXISTENT;
         }
     }
-    free(intname);
+    free(mboxname);
 
     kl = dlist_setfile(NULL, "MESSAGE", partition, &tmp_guid, sbuf.st_size, fname);
     sync_send_response(kl, sstate->pout);
@@ -3719,7 +3727,7 @@ int sync_restore_mailbox(struct dlist *kin,
                          struct sync_state *sstate)
 {
     /* fields from the request, all but mboxname are optional */
-    const char *mboxname;
+    char *mboxname;
     const char *uniqueid = NULL;
     const char *partition = NULL;
     const char *mboxtype = NULL;
@@ -3742,10 +3750,9 @@ int sync_restore_mailbox(struct dlist *kin,
     struct dlist *ki;
     int has_append = 0;
     int is_new_mailbox = 0;
-    char *intname;
     int r;
 
-    if (!dlist_getatom(kin, "MBOXNAME", &mboxname)) {
+    if (!dlist_getname_internal(kin, "MBOXNAME", &mboxname)) {
         syslog(LOG_DEBUG, "%s: missing MBOXNAME", __func__);
         return IMAP_PROTOCOL_BAD_PARAMETERS;
     }
@@ -3782,8 +3789,7 @@ int sync_restore_mailbox(struct dlist *kin,
      */
 
     /* open/create mailbox */
-    intname = mboxname_from_standard(mboxname);
-    r = mailbox_open_iwl(intname, &mailbox);
+    r = mailbox_open_iwl(mboxname, &mailbox);
     if (!r) r = sync_mailbox_version_check(&mailbox);
     syslog(LOG_DEBUG, "%s: mailbox_open_iwl %s: %s",
            __func__, mboxname, error_message(r));
@@ -3800,7 +3806,7 @@ int sync_restore_mailbox(struct dlist *kin,
             uidvalidity = 0;
         }
 
-        r = mboxlist_createsync(intname, mbtype, partition,
+        r = mboxlist_createsync(mboxname, mbtype, partition,
                                 sstate->userid, sstate->authstate,
                                 options, uidvalidity, createdmodseq,
                                 highestmodseq, acl,
@@ -3810,10 +3816,10 @@ int sync_restore_mailbox(struct dlist *kin,
 
         is_new_mailbox = 1;
     }
-    free(intname);
     if (r) {
         syslog(LOG_ERR, "Failed to open mailbox %s to restore: %s",
                mboxname, error_message(r));
+        free(mboxname);
         return r;
     }
 
@@ -3907,12 +3913,14 @@ int sync_restore_mailbox(struct dlist *kin,
         sync_log_append(mailbox->name);
 
     mailbox_close(&mailbox);
+    free(mboxname);
 
     return r;
 
 bail:
     mailbox_abort(mailbox);
     mailbox_close(&mailbox);
+    free(mboxname);
 
     return r;
 }
